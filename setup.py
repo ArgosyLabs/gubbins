fe1ce1fd2fe0679ca50b241fd1dd9f3d35bf3663
Derrick Lyndon Pallas <derrick@argosylabs.com>
2019-10-08 17:47:47 +0000

    v0.0.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name = "gubbins",
-    version = "0.0.2",
+    version = "0.0.3",
     description = ("Serial number generator/validator"),
     url = "https://github.com/ArgosyLabs/gubbins",
     author = "Derrick Lyndon Pallas",
